3523d3e650d9c455360fdedca29cd9263e77870c
Rijad Hodžić <123306816+rijadhodzic@users.noreply.github.com>
2024-06-28 03:03:19 +0200

    Create qa-badge-lang-de.php
    German translation.

---
+++ lang/qa-badge-lang-de.php
@@ -0,0 +1,259 @@
+<?php
+	return array(
+
+		'nice_question'=>'Schöne Frage',
+		'good_question'=>'Gute Frage',
+		'great_question'=>'Großartige Frage',
+
+		'notable_question'=>'Bemerkenswerte Frage',
+		'popular_question'=>'Beliebte Frage',
+		'famous_question'=>'Berühmte Frage',
+
+		'nice_answer'=>'Schöne Antwort',
+		'good_answer'=>'Gute Antwort',
+		'great_answer'=>'Großartige Antwort',
+
+		'nice_answer_old'=>'Erneuerung',
+		'good_answer_old'=>'Wiederbelebung',
+		'great_answer_old'=>'Wiederauferstehung',
+		
+		'nice_comment'=>'Schöner Kommentar',
+		'good_comment'=>'Guter Kommentar',
+		'great_comment'=>'Großartiger Kommentar',
+
+		'gifted'=>'Begabt',
+		'wise'=>'Weise',
+		'enlightened'=>'Erleuchtet',
+
+		'grateful'=>'Dankbar',
+		'respectful'=>'Respektvoll',
+		'reverential'=>'Ehrfurchtsvoll',
+						
+		'liked'=>'Gemocht',
+		'loved'=>'Geliebt',
+		'revered'=>'Verehrt',
+	
+		'asker'=>'Fragender',
+		'questioner'=>'Fragesteller',
+		'inquisitor'=>'Befrager',
+
+		'answerer'=>'Antwortgeber',
+		'lecturer'=>'Dozent',
+		'preacher'=>'Prediger',
+
+		'commenter'=>'Kommentator',
+		'commentator'=>'Kommentator',
+		'annotator'=>'Kommentator',
+					
+		'voter'=>'Wähler',
+		'avid_voter'=>'Eifriger Wähler',
+		'devoted_voter'=>'Hingebungsvoller Wähler',
+
+		'editor'=>'Redakteur',
+		'copy_editor'=>'Lektor',
+		'senior_editor'=>'Herausgeber',
+		
+		'watchdog'=>'Wachhund',
+		'bloodhound'=>'Spürhund',
+		'pitbull'=>'Pitbull',
+
+		'reader'=>'Leser',
+		'avid_reader'=>'Eifriger Leser',
+		'devoted_reader'=>'Hingebungsvoller Leser',
+		
+		'dedicated'=>'Engagiert',
+		'devoted'=>'Hingebungsvoll',
+		'zealous'=>'Eifrig',
+
+		'visitor'=>'Besucher',
+		'trouper'=>'Alter Hase',
+		'veteran'=>'Veteran',
+
+		'regular'=>'Stammgast',
+		'old_timer'=>'Oldtimer',
+		'ancestor'=>'Ahne',
+		
+		'100_club'=>'100er Club',
+		'1000_club'=>'1000er Club',
+		'10000_club'=>'10.000er Club',
+		
+		'medalist'=>'Medailleur',
+		'champion'=>'Champion',
+		'olympian'=>'Olympionike',
+
+		'verified'=>'Verifizierter Mensch',
+		'profiler'=>'Autobiograph',
+		'avatar'=>'Fotogen',
+		
+		'nice_question_desc'=>'Frage erhielt +# Upvotes',
+		'good_question_desc'=>'Frage erhielt +# Upvotes',
+		'great_question_desc'=>'Frage erhielt +# Upvotes',
+
+		'notable_question_desc'=>'Gestellte Frage erhielt # ^view^Views',
+		'popular_question_desc'=>'Gestellte Frage erhielt # ^view^Views',
+		'famous_question_desc'=>'Gestellte Frage erhielt # ^view^Views',
+		
+		'nice_answer_desc'=>'Antwort erhielt +# Upvotes',
+		'good_answer_desc'=>'Antwort erhielt +# Upvotes',
+		'great_answer_desc'=>'Antwort erhielt +# Upvotes',
+
+		'nice_comment_desc'=>'Kommentar erhielt +# Upvotes',
+		'good_comment_desc'=>'Kommentar erhielt +# Upvotes',
+		'great_comment_desc'=>'Kommentar erhielt +# Upvotes',
+		
+		'nice_answer_old_desc'=>'Erhielt $nice_answer Abzeichen als Antwort auf eine Frage, die älter als # ^day^Tage ist',
+		'good_answer_old_desc'=>'Erhielt $good_answer Abzeichen als Antwort auf eine Frage, die älter als # ^day^Tage ist',
+		'great_answer_old_desc'=>'Erhielt $great_answer Abzeichen als Antwort auf eine Frage, die älter als # ^day^Tage ist',
+		
+		'gifted_desc'=>'# ^answer^Antworten als beste Antwort ausgewählt',
+		'wise_desc'=>'# ^answer^Antworten als beste Antwort ausgewählt',
+		'enlightened_desc'=>'# ^answer^Antworten als beste Antwort ausgewählt',
+
+		'grateful_desc'=>'# ^answer^Antworten als beste Antwort ausgewählt',
+		'respectful_desc'=>'# ^answer^Antworten als beste Antwort ausgewählt',
+		'reverential_desc'=>'# ^answer^Antworten als beste Antwort ausgewählt',
+
+		'liked_desc'=>'Erhielt insgesamt # ^upvote^Upvotes',
+		'loved_desc'=>'Erhielt insgesamt # ^upvote^Upvotes',
+		'revered_desc'=>'Erhielt insgesamt # ^upvote^Upvotes',
+	
+		'asker_desc'=>'Stellte # ^question^Fragen',
+		'questioner_desc'=>'Stellte # ^question^Fragen',
+		'inquisitor_desc'=>'Stellte # ^question^Fragen',
+
+		'answerer_desc'=>'Veröffentlichte # ^answer^Antworten',
+		'lecturer_desc'=>'Veröffentlichte # ^answer^Antworten',
+		'preacher_desc'=>'Veröffentlichte # ^answer^Antworten',
+
+		'commenter_desc'=>'Veröffentlichte # ^comment^Kommentare',
+		'commentator_desc'=>'Veröffentlichte # ^comment^Kommentare',
+		'annotator_desc'=>'Veröffentlichte # ^comment^Kommentare',
+		
+		'voter_desc'=>'Abgestimmt # ^time^Mal',
+		'avid_voter_desc'=>'Abgestimmt # ^time^Mal',
+		'devoted_voter_desc'=>'Abgestimmt # ^time^Mal',
+
+		'editor_desc'=>'Insgesamt # ^edit^Bearbeitungen durchgeführt',
+		'copy_editor_desc'=>'Insgesamt # ^edit^Bearbeitungen durchgeführt',
+		'senior_editor_desc'=>'Insgesamt # ^edit^Bearbeitungen durchgeführt',
+		
+		'watchdog_desc'=>'# ^post^Beiträge als unangemessen markiert',
+		'bloodhound_desc'=>'# ^post^Beiträge als unangemessen markiert',
+		'pitbull_desc'=>'# ^post^Beiträge als unangemessen markiert',
+
+		'reader_desc'=>'Insgesamt # ^question^Fragen gelesen',
+		'avid_reader_desc'=>'Insgesamt # ^question^Fragen gelesen',
+		'devoted_reader_desc'=>'Insgesamt # ^question^Fragen gelesen',
+		
+		'dedicated_desc'=>'Jeden Tag für # aufeinanderfolgende ^day^Tage besucht',
+		'devoted_desc'=>'Jeden Tag für # aufeinanderfolgende ^day^Tage besucht',
+		'zealous_desc'=>'Jeden Tag für # aufeinanderfolgende ^day^Tage besucht',
+
+		'visitor_desc'=>'Besuchte die Seite insgesamt an # ^day^Tagen',
+		'trouper_desc'=>'Besuchte die Seite insgesamt an # ^day^Tagen',
+		'veteran_desc'=>'Besuchte die Seite insgesamt an # ^day^Tagen',
+
+		'regular_desc'=>'Zum ersten Mal vor mehr als # ^day^Tagen besucht',
+		'old_timer_desc'=>'Zum ersten Mal vor mehr als # ^day^Tagen besucht',
+		'ancestor_desc'=>'Zum ersten Mal vor mehr als # ^day^Tagen besucht',
+
+		'100_club_desc'=>'Erhielt insgesamt # ^point^Punkte',
+		'1000_club_desc'=>'Erhielt insgesamt # ^point^Punkte',
+		'10000_club_desc'=>'Erhielt insgesamt # ^point^Punkte',
+
+		'medalist_desc'=>'Erhielt insgesamt # ^badge^Abzeichen',
+		'champion_desc'=>'Erhielt insgesamt # ^badge^Abzeichen',
+		'olympian_desc'=>'Erhielt insgesamt # ^badge^Abzeichen',
+
+		'verified_desc'=>'E-Mail-Adresse erfolgreich verifiziert',
+		'profiler_desc'=>'Alle Profilfelder ausgefüllt',
+		'avatar_desc'=>'Ein Avatar-Bild hochgeladen',
+
+		'bronze'=>'Bronze',
+		'silver'=>'Silber',
+		'gold'=>'Gold',
+
+		'bronze_desc'=>'Bronzene Abzeichen werden für die grundlegende Teilnahme vergeben. Sie sind relativ leicht zu verdienen, wenn man teilnimmt.',
+		'silver_desc'=>'Silberne Abzeichen werden für eine erweiterte Teilnahme vergeben. Sie sind eine Leistung und ein Zeichen für echte Teilnahme.',
+		'gold_desc'=>'Goldene Abzeichen werden für außergewöhnliche Teilnahme vergeben. Nur langfristige Teilnahme kann dir diese einbringen. Sie sind ein Zeichen für einen wirklich wertvollen Beitrag!',
+		
+		'badge_admin'=>'Abzeichen-Admin',
+		'badges'=>'Abzeichen',
+		'badge_list_title'=>'Abzeichenliste',
+		
+		'badge_count_click'=>'Klicken, um Quelle(n) zu sehen',
+		
+		'badge_list_pre'=>'Folgend ist eine Liste aller verfügbaren Abzeichen auf dieser Seite:',
+		'badge_name'=>'Abzeichenname',
+		'badge_description'=>'Abzeichenbeschreibung',
+		'awarded'=>'vergeben',
+		'awarded_total'=>'an die Gemeinschaft vergeben.',
+		'badges_total'=>'insgesamt Abzeichen',
+		
+		'badge_notify'=>'Glückwunsch! Du hast ein Abzeichen erhalten ',
+		'badge_notify_multi_plural'=>'und # weitere Abzeichen!',
+		'badge_notify_multi_singular'=>'und 1 weiteres Abzeichen!',
+		'badge_notify_profile_pre'=>'Schau dir ',
+		'badge_notify_profile'=>'dein Profil an',
+		
+		'badge_names_reset'=>'Abzeichennamen zurückgesetzt.',
+		'badge_values_reset'=>'Abzeichenwerte zurückgesetzt.',
+		'badge_list_rebuilt'=>'Abzeichenliste neu erstellt.',
+		'badge_admin_saved'=>'Optionen gespeichert.',
+		
+		'badge_admin_activate'=>'Abzeichen aktivieren',
+		'active_badges'=>'Aktive Abzeichen',
+		'badge_award_button'=>'Abzeichenzuweisung neu überprüfen',
+		'badge_award_button_desc'=>'Manuell alle verdienten Auszeichnungen überprüfen',
+		'badge_award_delete_desc'=>'Zuerst alle Abzeichen aus der Datenbank entfernen',
+		'badge_recreate'=>'Abzeichen neu erstellen',
+		'badge_recreate_desc'=>'Abzeichendatenbank neu erstellen (bei Änderungen am PHP-Code)',
+		
+		'badge_admin_click_edit'=>'Klicken, um Abzeichenname zu bearbeiten',
+		'badge_admin_select_all'=>'Alle auswählen/keine',
+
+		'badge_reset_css'=>'CSS zurücksetzen',
+		'badge_reset_values'=>'Werte zurücksetzen',
+		'badge_reset_values_desc'=>'Variable Abzeichendaten wie oben eingegeben zurücksetzen',
+		'badge_reset_names'=>'Namen zurücksetzen',
+		'badge_reset_names_desc'=>'Abzeichennamen wie oben eingegeben zurücksetzen',
+		
+		'notify_time'=>'Benachrichtigungsdauer',
+		'notify_time_desc'=>'Dauer in Sekunden, bevor die Abzeichenbenachrichtigung ausgeblendet wird (0 = aus)',
+		'badge_trigger_notify'=>'Testbenachrichtigung',
+		'badge_trigger_notify_desc'=>'Zeigt eine Beispiel-Benachrichtigungsbox',
+		'badge_admin_user_field'=>'Abzeichen zu Profilseiten hinzufügen',
+		'badge_admin_user_field_no_tab'=>'Abzeichen im Hauptprofilfeld anzeigen (nicht im Abzeichentab)',
+		'badge_show_source_posts'=>'Liste der Quellenbeiträge für jedes Abzeichen im Benutzerprofil anzeigen',
+		'badge_show_source_users'=>'Liste der ausgezeichneten Benutzer für jedes Abzeichen auf der Abzeichenseite anzeigen',
+		'badge_admin_user_widget'=>'Abzeichen-Widget neben Punkten im Benutzermeta anzeigen',
+		'badge_admin_user_widget_q_item'=>'Abzeichen-Widget neben Punkten in der Fragenliste anzeigen',
+		'badge_admin_loggedin_widget'=>'Abzeichen-Widget neben Punkten für eingeloggte Benutzer anzeigen',
+		'badge_show_users_badges'=>'Abzeichen-Widget neben Benutzernamen auf der Benutzerlistenseite anzeigen',
+
+		'badge_email_notify'=>'Benutzer per E-Mail über verliehene Abzeichen benachrichtigen',
+		'badge_email_notify_on'=>'E-Mail-Benachrichtigung standardmäßig aktiviert',
+		'badge_email_subject'=>'Nachrichtenbetreff',
+		'badge_email_body'=>'Nachrichtentext',
+		'badge_notify_email_me'=>'Benachrichtigen Sie mich über neue Abzeichen per E-Mail?',
+		'badge_notified_email_me'=>'Benachrichtigungseinstellungen gespeichert.',
+		
+		'badge_widget_title' => 'Kürzliche Abzeichen',
+		'widget_list_max' => 'Maximale Anzahl der kürzlichen Abzeichen im Widget',
+		'widget_date_max' => 'Maximales Alter der Abzeichen im Widget',
+
+		'save_settings'=>'Speichern',
+		'save_settings_desc'=>'Obige Einstellungen speichern',
+		
+		/* New stuff */
+		'badge_of_type'=>'vom Typ (ausgenommen wiederholte Abzeichen)',
+		'badge_empty_source'=>'Gelöschter Beitrag',
+		'badge_anonymous_user'=>'Keine Daten',
+		'close_badge_source'=>'Schließen',
+		'widget_badge_earned'=>'Abzeichen erhalten',
+		'widget_badge_earned_ago'=>'vor' // Beispiel: vor 3 Monaten
+	);
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
